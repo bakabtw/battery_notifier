25968ae661b97b2b79ee6b518d520e05b69b25e2
Eugene Konstantinov <me@knst.me>
2022-07-03 16:07:55 +0300

    Added support for command line arguments

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import time
 from plyer import notification
+import argparse
 
 
 def battery_status():
@@ -17,10 +18,40 @@ def battery_status():
 
 
 def main():
-    # Settings
-    lowCharge = 40
+    # Default values
+    lowCharge = 30
     highCharge = 70
     remindInterval = 300
+
+    parser = argparse.ArgumentParser(prog="battery_notifier.py", description="Process some integers")
+    parser.add_argument("--min", help=f"Min battery level. Default: {lowCharge}", type=int, default=lowCharge)
+    parser.add_argument("--max", help=f"Max battery level. Default: {highCharge}", type=int, default=highCharge)
+    parser.add_argument("--delay", help=f"Delay between checks. Default: {remindInterval}", type=int, default=remindInterval)
+    args = parser.parse_args()
+
+    lowCharge, highCharge, remindInterval = args.min, args.max, args.delay
+
+    if lowCharge not in range(1, 100):
+        print("Battery min level is out of range 1-100",
+              f"Current value: {lowCharge}",
+              sep="\n"
+              )
+        exit()
+    if highCharge not in range(1, 100):
+        print(f"Battery max level is out of range 1-100",
+              f"Current value: {highCharge}",
+              sep="\n"
+              )
+        exit()
+    if lowCharge >= highCharge:
+        print("Battery max level cannot be higher than battery min level",
+              f"Min charger level: {lowCharge}",
+              f"Max charge level: {highCharge}",
+              sep="\n"
+              )
+        exit()
+    if remindInterval < 1:
+        print("Delay between checks should be >= 1 sec")
 
     while True:
         currentCapacity, batteryStatus = battery_status()
